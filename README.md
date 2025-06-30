f56f3d7b193ccccd737dc5a5d735ec4ce370e8cf
aboudev22 <aboubakarjay2@gmail.com>
2025-06-30 12:52:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Vite
+
+# Tailwindcss + Framer-motion
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      App
+    </div>
+  );
+}
