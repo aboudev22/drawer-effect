407bc73462f4c15f31695064e397b8ac3256448e
aboudev22 <aboubakarjay2@gmail.com>
2025-06-30 18:27:12 +0100

    drag added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,70 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useRef, useState } from "react";
+
 export default function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const sectionRef = useRef<HTMLDivElement | null>(null);
+  const dragRef = useRef<HTMLDivElement | null>(null);
+  const [topConstraint, setTopConstraint] = useState(dragRef);
+
+  const handleDragEnd = (
+    _: MouseEvent | PointerEvent | TouchEvent,
+    info: { offset: { y: number; x: number } }
+  ) => {
+    if (info.offset.y > 50) {
+      setIsOpen(false);
+    }
+    if (info.offset.y < -50) {
+      setTopConstraint(sectionRef);
+    }
+  };
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      App
+    <div className="realtive h-screen w-screen flex justify-center items-center bg-white">
+      <motion.button
+        onClick={() => {
+          setTopConstraint(dragRef);
+          setIsOpen(!isOpen);
+        }}
+        whileTap={{ scale: 0.9 }}
+        className="text-sm font-bold px-4 py-2 rounded-md cursor-pointer hover:bg-neutral-200 border-[1px] border-neutral-300"
+      >
+        open drawer
+      </motion.button>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            ref={sectionRef}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            // onClick={() => setIsOpen(!isOpen)}
+            className="absolute flex flex-col justify-end bg-black/20 left-0 right-0 h-full overflow-hidden"
+          >
+            <motion.div
+              drag="y"
+              ref={dragRef}
+              onDragEnd={handleDragEnd}
+              dragConstraints={topConstraint}
+              onClick={(e) => e.stopPropagation()}
+              initial={{ y: "100%", opacity: 0 }}
+              animate={{
+                y: 0,
+                opacity: 1,
+                height: topConstraint === dragRef ? "50%" : "100%",
+              }}
+              exit={{ y: "100%", opacity: 0 }}
+              transition={{ ease: "linear" }}
+              className="flex flex-col bg-white rounded-xl"
+            >
+              <div className="w-full flex justify-center items-center cursor-grab active:cursor-grabbing p-2">
+                <button className="w-12 h-2 bg-neutral-300 mt-2 rounded-full" />
+              </div>
+              <div className="h-full w-full bg-neutral-200"></div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
